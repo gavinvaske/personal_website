b0ce91ceaece7deb742b3007d4788323e20dc187
Gavin Vaske <Gavin.Vaske@gmail.com>
2022-12-24 16:41:50 -0600

    refactor directory structure; add basic sass styling to component

--- application/components/header/Header.js
+++ application/components/header/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import './header.scss';
 
 const Header = () => {
   return (
-    <h1>
-        Portfolio Websitee
+    <h1 className='header-h1'>
+        Gavin Vaske's Portfolio Website
     </h1>
   )
 }
